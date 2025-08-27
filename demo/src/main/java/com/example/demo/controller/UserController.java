1b9d021c20e5fc79255e519b8752b29e9a2f6d8b
Zavtra <58968586+Zavtra8@users.noreply.github.com>
2025-08-27 15:56:48 +0800

    Update UserController.java

--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -4,12 +4,19 @@ import com.example.demo.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/api/users")
 @Tag(name = "User API", description = "Operations related to users")
 public class UserController {
     private final UserService userService;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -19,6 +26,18 @@ public class UserController {
     @Operation(summary = "Get all users", description = "Returns a list of all registered users")
     public String getUsers() {
         return userService.getAllUsers("Red dates");
+    }
+
+    @GetMapping("/callMessage")
+    public String callMessage() {
+        String url = "http://localhost:8081/api/messages/getMessage"; // Update with actual host/port
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    @GetMapping("/callMessage2")
+    public String callMessage2() {
+        String url = "http://localhost:8081/api/messages/getMessage2"; // Update with actual host/port
+        return restTemplate.getForObject(url, String.class);
     }
 
     @PostMapping("/createUser")
